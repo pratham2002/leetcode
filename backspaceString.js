3e256772d3f67ce8bc6b9f8f66c66b8fe400f702
Pratham <agrawalpratham2002@gmail.com>
2023-10-19 23:56:39 +0530

    backSpaceString

---
+++ backspaceString.js
@@ -0,0 +1,25 @@
+function backspaceCompare(s, t) {
+  let stack1 = [];
+  let stack2 = [];
+  let backspaceChar = "#";
+
+  for (let i = 0; i < s.length; i++) {
+    const element = s[i];
+    if (element === backspaceChar) {
+      stack1.pop();
+    } else {
+      stack1.push(element);
+    }
+  }
+  for (let i = 0; i < t.length; i++) {
+    const element = t[i];
+    if (element === backspaceChar) {
+      stack2.pop();
+    } else {
+      stack2.push(element);
+    }
+  }
+  return stack1.length === stack2.length;
+}
+
+console.log(backspaceCompare("ab##", "c#d#"));
