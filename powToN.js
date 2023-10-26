e8e96e4f0027bd022ee4456e1d04442f1d2afd18
Pratham <agrawalpratham2002@gmail.com>
2023-10-26 22:31:05 +0530

    50

--- powToN.js
+++ powToN.js
@@ -1,3 +1,19 @@
+// var myPow = function (x, n) {
+//   return (x ** n).toFixed(5);
+// };
 var myPow = function (x, n) {
-  return (x ** n).toFixed(5);
+  let result = 1;
+  if (n < 0) (x = 1 / x), (n *= -1);
+  while (n > 0) {
+    if (n % 2 == 0) {
+      x = x * x;
+      n = Math.floor(n / 2);
+    } else {
+      result = result * x;
+      n = n - 1;
+    }
+  }
+  return result;
 };
+
+console.log(myPow(0.00001, 2147483647));
